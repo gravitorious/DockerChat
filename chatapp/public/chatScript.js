c995bdbe126f6636edcdeaf243e8710fa32fe77a
Mavrogeneiadis Nikolaos <nikolasmay1@yahoo.gr>
2020-06-10 14:47:23 +0300

    Add namespaces

--- chatapp/public/chatScript.js
+++ chatapp/public/chatScript.js
@@ -1,17 +1,36 @@
-const socket = io('http://localhost:9000'); //create the connection, returns the socket
+const socket = io('http://localhost:9000'); // /endpoint
+
+
+/* me to pou kanei syndesh
+socket.on('connect', ()=>{
+
+})*/
+
+//listener for nsList
+socket.on('nsList', (nsData)=>{
+    let namespacesDiv = document.querySelector('.namespaces');
+    namespacesDiv.innerHTML = "";
+    nsData.forEach((ns)=>{
+        namespacesDiv.innerHTML += `<div class="namespace" ns=${ns.endpoint}><img src="${ns.img}" /></div>`
+    })
+
+    //click listener for each namespace
+
+    Array.from(document.getElementsByClassName('namespace')).forEach((elem)=>{
+        elem.addEventListener('click', (ev)=>{
+            const nsEndPoint = elem.getAttribute('ns');
+        })
+    })
+})
+
 socket.on('messageFromServer', (dataFromServer)=>{
     console.log(dataFromServer);
     socket.emit('dataToServer', {data: "Data from the client!"})
 })
 
+
 document.querySelector('#message-form').addEventListener('submit', (event)=>{
     event.preventDefault();
     const newMessage = document.querySelector('#user-message').value;
-    console.log(newMessage)
     socket.emit('newMessageToServer', {text:newMessage})
-})
-
-socket.on('newMessageToClients', (msg)=>{
-    console.log(msg)
-    document.querySelector('#messages').innerHTML += `<li>${msg.text}</li>`
 })
